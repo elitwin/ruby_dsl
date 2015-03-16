f121b72769b8c25802ad0127b2d75b9d99bea6c6
Eric Litwin <eric.litwin@pnmac.com>
2015-03-16 14:33:19 -0700

    Added some clarifying comments

--- has_many.rb
+++ has_many.rb
@@ -7,9 +7,12 @@ module HasMany
     rescue NameError
       method_module = Module.new
       self.const_set(mod_name, method_module)
+      # adds as instance methods in the class
       include method_module
     end
 
+    # include line_no on same line as method_defs to provide
+    # correct location of any errors in the stack trace
     line_no = __LINE__; method_defs = %{
       def #{name}
         driver.find_elements(:css, '.#{name}')
@@ -19,6 +22,7 @@ module HasMany
         #{name}.map(&:text)
       end
     }
+    # evaluate the string and turn it into real Ruby methods
     method_module.module_eval method_defs, __FILE__, line_no
   end
 end
@@ -29,5 +33,6 @@ class Page
   has_many :features
 end
 
-puts Page.instance_methods.sort
+puts "Methods: #{Page.instance_methods.sort}"
+puts "Ancestors: #{Page.ancestors}"
 #puts Page.new.features_texts
