9a9b9a4ea2c6c3e3e917b4a2e52acd01f3631d3e
Eric Litwin <eric.litwin@pnmac.com>
2015-03-16 15:35:42 -0700

    Added compile lambda to help with development

--- has_many.rb
+++ has_many.rb
@@ -16,16 +16,30 @@ end
 module HasMany
   def has_many(name)
     method_module = ModuleHelper.module_for('HasMany', name, self)
+
+    # Lambda method for compiling code to get better syntax errors
+    compile = ->(code, line_no){
+      begin
+        method_module.module_eval(code, __FILE__, line_no)
+      rescue SyntaxError
+        raise(SyntaxError, "\n#{ code }\n")
+      end
+    }
+
     # evaluate the string and turn it into real Ruby methods
-    method_module.module_eval <<-CODE, __FILE__, __LINE__
+    line_no = __LINE__; code = <<-CODE
       def #{name}
         driver.find_elements(:css, '.#{name}')
       end
+    CODE
+    compile[code, line_no] #compile.call(code, line_no) - alternate syntax
 
+    line_no = __LINE__; code = <<-CODE
       def #{name}_texts
         #{name}.map(&:text)
       end
     CODE
+    compile[code, line_no]
   end
 end
 
