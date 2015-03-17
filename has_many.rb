9b460f35f68f1f0b86fa559dc1ceb747af99baae
Eric Litwin <eric.litwin@pnmac.com>
2015-03-17 11:23:04 -0700

    Refactored ModuleHelper to separate file

--- has_many.rb
+++ has_many.rb
@@ -1,17 +1,4 @@
-module ModuleHelper
-  module_function
-  def module_for(prefix, name, klass)
-    mod_name = prefix + name.to_s.split('_').map(&:capitalize).join
-    begin
-      method_module = klass.send(:const_get, mod_name)
-    rescue NameError
-      method_module = Module.new
-      klass.send(:const_set, mod_name, method_module)
-      # adds as instance methods in the class
-      klass.send(:include, method_module)
-    end
-  end
-end
+require_relative 'module_helper'
 
 module HasMany
   def has_many(name)
